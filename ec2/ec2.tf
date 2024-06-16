8cc79cdc4ddf525b1d45bf5c4bb3a7cb7603f5e7
Swetha <swetha.s87@gmail.com>
2024-06-16 13:55:53 +0100

    ec2 main

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow SSH inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.main.id
+
 
   ingress {
     from_port        = 22
